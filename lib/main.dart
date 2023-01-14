c6b8a81976385bd058be65831650d1e384d957a2
Daniel Newton <djpnewton@gmail.com>
2023-01-14 21:02:51 +1300

    select market

--- lib/main.dart
+++ lib/main.dart
@@ -29,19 +29,73 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
+  final _coinData = CoinData();
   bool _darkMode = true;
-  bool _showAverage = false;
-  bool _retreivingData = true;
+  List<Market> _markets = [];
+  Market _market = Market.empty();
+  bool _showMa200 = false;
+  final List<String> _intervals = ['5m', '1h', '1D', '1W', '1M'];
+  List<bool> _selectedInterval = [false, false, true, false, false];
+  bool _haveData = false;
+  bool _retreivingData = false;
 
   @override
   void initState() {
-    btcData().then((value) {
-      log.info('retrieved data');
-      var model = context.read<ChartModel>();
-      model.updateData(value);
-      setState(() => _retreivingData = false);
+    _coinData.markets().then((value) {
+      setState(() => _markets = value);
+      if (value.isNotEmpty) {
+        _setMarket(value[0]);
+      }
     });
     super.initState();
+  }
+
+  Widget _makeControls() {
+    if (_markets.isEmpty) {
+      return Row(children: [
+        Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text(_coinData.source())),
+        const CircularProgressIndicator()
+      ]);
+    }
+    return Row(children: [
+      const SizedBox(width: 10),
+      Text(_coinData.source()),
+      const SizedBox(width: 10),
+      DropdownButton<String>(
+          items: _markets
+              .map((e) => DropdownMenuItem<String>(
+                  value: e.symbol,
+                  child: Text('${e.baseAsset}-${e.quoteAsset}')))
+              .toList(),
+          value: _market.symbol,
+          onChanged: _marketChange),
+      const SizedBox(width: 10),
+      ToggleButtons(
+        isSelected: [_showMa200],
+        onPressed: (int index) => _toggleMa200(),
+        children: const [Text('MA 200')],
+      ),
+      const SizedBox(width: 10),
+      ToggleButtons(
+        isSelected: _selectedInterval,
+        onPressed: (int index) {
+          for (int buttonIndex = 0;
+              buttonIndex < _selectedInterval.length;
+              buttonIndex++) {
+            if (buttonIndex == index) {
+              _selectedInterval[buttonIndex] = true;
+            } else {
+              _selectedInterval[buttonIndex] = false;
+            }
+          }
+          setState(() => _selectedInterval = _selectedInterval);
+          _updateCandles(_market, _interval());
+        },
+        children: _intervals.map((e) => Text(e)).toList(),
+      ),
+    ]);
   }
 
   @override
@@ -52,29 +106,74 @@ class MyAppState extends State<MyApp> {
           brightness: _darkMode ? Brightness.dark : Brightness.light,
         ),
         home: Scaffold(
-            appBar: AppBar(title: const Text('Basic BTC Chart'), actions: [
-              IconButton(
-                icon: Icon(_darkMode ? Icons.dark_mode : Icons.light_mode),
-                onPressed: () => setState(() => _darkMode = !_darkMode),
-              ),
-              IconButton(
-                icon: Icon(
-                  _showAverage ? Icons.show_chart : Icons.bar_chart_outlined,
-                ),
-                onPressed: () {
-                  _showAverage = !_showAverage;
-                  var model = context.read<ChartModel>();
-                  if (_showAverage) {
-                    model.computeTrendLines();
-                  } else {
-                    model.removeTrendLines();
-                  }
-                },
-              ),
-            ]),
-            body: _retreivingData
-                ? const CircularProgressIndicator()
-                : Consumer<ChartModel>(
-                    builder: (context, model, child) => Chart(model))));
+          appBar: AppBar(title: const Text('Basic BTC Chart'), actions: [
+            IconButton(
+              icon: Icon(_darkMode ? Icons.dark_mode : Icons.light_mode),
+              onPressed: () => setState(() => _darkMode = !_darkMode),
+            ),
+          ]),
+          body: Column(children: [
+            _makeControls(),
+            _retreivingData
+                ? const Center(child: CircularProgressIndicator())
+                : _haveData
+                    ? Expanded(
+                        child: Consumer<ChartModel>(
+                            builder: (context, model, child) => Chart(model)))
+                    : const Text('no data to show')
+          ]),
+        ));
+  }
+
+  void _setMarket(Market market) {
+    setState(() => _market = market);
+    _updateCandles(market, _interval());
+  }
+
+  void _marketChange(String? symbol) {
+    for (var market in _markets) {
+      if (market.symbol == symbol) {
+        _setMarket(market);
+        return;
+      }
+    }
+  }
+
+  String _interval() {
+    for (int i = 0; i < _selectedInterval.length; i++) {
+      if (_selectedInterval[i]) {
+        return _intervals[i];
+      }
+    }
+    log.severe('could not find valid interval');
+    return _intervals[0];
+  }
+
+  void _updateCandles(Market market, String interval) {
+    setState(() => _retreivingData = true);
+    _coinData.candles(market.symbol, interval).then((value) {
+      log.info('retrieved data for ${market.symbol}');
+      var model = context.read<ChartModel>();
+      model.updateData(value);
+      _updateMa200();
+      setState(() {
+        _retreivingData = false;
+        _haveData = true;
+      });
+    });
+  }
+
+  void _toggleMa200() {
+    setState(() => _showMa200 = !_showMa200);
+    _updateMa200();
+  }
+
+  void _updateMa200() {
+    var model = context.read<ChartModel>();
+    if (_showMa200) {
+      model.computeMa200();
+    } else {
+      model.removeTrendLines();
+    }
   }
 }
